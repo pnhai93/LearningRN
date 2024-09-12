53fd6a879cfb1902e46fcf87f89779f49a95c819
Tran Duc Thinh <ducthinh1341993@gmail.com>
2024-09-12 14:45:47 +0700

    firt commit ignite v9.10.0

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,24 @@
 /** @type {import('@babel/core').TransformOptions['plugins']} */
-const plugins = [  
+const plugins = [
   /** react-native-reanimated web support @see https://docs.swmansion.com/react-native-reanimated/docs/guides/web-support/ */
   "@babel/plugin-transform-export-namespace-from",
+  [
+    "module-resolver",
+    {
+      root: ["./app"],
+      extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+    },
+  ],
 ]
 
 /** @type {import('@babel/core').TransformOptions} */
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ["babel-preset-expo"],
     env: {
       production: {},
     },
     plugins,
-  };
-};
+  }
+}
